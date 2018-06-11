57c4b4f714e77b4875b985cb547f7c0cf95ab6a4
Ben Firshman <ben@firshman.co.uk>
2018-06-11 19:19:59 +0300

    Add server for development

---
+++ src/server.js
@@ -0,0 +1,21 @@
+const Bundler = require("parcel-bundler");
+const app = require("express")();
+const render = require("./index").render;
+const path = require("path");
+const tmp = require("tmp-promise");
+
+module.exports.start = async input => {
+  const tmpDir = await tmp.dir();
+  const htmlPath = await render({
+    input: input,
+    output: tmpDir.path,
+    // This absolute filesystem path will be passed unmodified through
+    // to the parcel bundler, which will compile the SCSS
+    externalCSS: path.join(__dirname, "css/index.scss")
+  });
+
+  console.log("💅  Starting server at http://localhost:8000");
+  const bundler = new Bundler(htmlPath, { hmrPort: 8001 });
+  app.use(bundler.middleware());
+  app.listen(8000);
+};
